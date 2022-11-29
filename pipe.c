dfb6e82fc56bfa23b0dc24bf2a73920dd9d856e6
tpeters <tpeters@student.42heilbronn.de>
2022-11-30 00:08:09 +0100

    pipe works with arbitrary amount of pipes (change the N)

--- pipe.c
+++ pipe.c
@@ -4,58 +4,74 @@
 #include <unistd.h>
 #include <errno.h>
 #include "libft/libft.h"
+#include <sys/wait.h>
+
+//number of pipes
+#define N 3
+
+void	close_fds(int pos, int *fd)
+{
+	while (pos < N * 2)
+	{
+		close(fd[pos]);
+		pos++;
+	}
+}
 
 int main(int argc, char **argv)
 {
-    int fd1[2];
-    int fd2[2];
-    char buf[] = "HELLO WORLD!";
-    if(pipe(fd1)){
-      perror("pipe");
-      return -1;
-    }
-	if(pipe(fd2)){
-      perror("pipe");
-      return -1;
-    }
-    switch(fork()){
-        case -1:
-            perror("fork");
-            return -1;
-        case 0:
-            // child
-            close(fd1[1]);
-            dup2(fd1[0], STDIN_FILENO);
-            close(fd1[0]);
-			dup2(fd2[1], STDOUT_FILENO);
-			close(fd2[1]);
-			close(fd2[0]);
-            execve("./cat", NULL, NULL);
-        default:
-            // parent
-            close(fd1[0]);
-			char *tmp = get_next_line(0);
-            write(fd1[1], tmp, ft_strlen(tmp));
-			free(tmp);
-            close(fd1[1]);
-            wait(NULL);
-    }
-	switch(fork()){
-        case -1:
-            perror("fork");
-            return -1;
-        case 0:
-            // child
-            close(fd2[1]);
-            dup2(fd2[0], STDIN_FILENO);
-            close(fd2[0]);
-            execve("./cat", NULL, NULL);
-        default:
-            // parent
-            close(fd2[0]);
-            close(fd2[1]);
-            wait(NULL);
-    }
-    printf("END~\n");
-    return 0;
+	int	*fd;
+	int	i;
+	int	pid;
+	int	forks;
+
+	fd = (int *)calloc(sizeof(int), N * 2);
+	if (!fd)
+		return (-1);
+	i = 0;
+	while (i < N)
+	{
+		if(pipe(fd + i * 2)){
+			perror("pipe");
+			return -1;
+		}
+		i++;
+	}
+	forks = 0;
+	while (forks < N)
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			return -1;
+		}
+		else if (pid == 0)
+		{
+			// child
+			close(fd[forks * 2 + 1]);
+			dup2(fd[forks * 2 + 0], STDIN_FILENO);
+			close(fd[forks * 2 + 0]);
+			if (forks != N - 1)
+				dup2(fd[(forks + 1) * 2 + 1], STDOUT_FILENO);
+			close_fds(forks * 2 + 0, fd);
+			execve("./cat", NULL, NULL);
+		}
+		else
+		{
+			// parent
+			if (forks == 0)
+			{
+				char *tmp = get_next_line(0);
+				write(fd[forks * 2 + 1], tmp, ft_strlen(tmp));
+				free(tmp);
+			}
+			close(fd[forks * 2 + 0]);
+			close(fd[forks * 2 + 1]);
+			waitpid(pid, NULL, 0);
+		}
+		forks++;
+	}
+	free(fd);
+	return 0;
 }
