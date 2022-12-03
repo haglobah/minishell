3ec6ab3f180605ab38c94ac9132578ecd6ca9c87
Beat Hagenlocher <bah@posteo.de>
2022-12-03 14:25:46 +0100

    Add echo, pwd & env as builtins

--- builtins.c
+++ builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bhagenlo <bhagenlo@student.42heil...>      +#+  +:+       +#+        */
+/*   By: bhagenlo <bhagenlo@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 17:36:28 by bhagenlo          #+#    #+#             */
-/*   Updated: 2022/12/02 17:36:28 by bhagenlo         ###   ########.fr       */
+/*   Updated: 2022/12/03 14:24:56 by bhagenlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 
 int	ft_echo(t_msh *m, char **args)
 {
-	//arg handling
+	int	argc;
+	int	i;
+	int	n_flag;
+
+	n_flag = 0;
+	i = 1;
+	argc = strslen(args);
+	if (s_iseq(args[1], "-n"))
+	{
+		i++;
+		n_flag = 1;
+	}
+	while (args[i])
+	{
+		ft_printf("%s", args[i]);
+		if (!n_flag || (n_flag && i < argc - 1))
+			ft_printf("\n");
+		i++;
+	}
 	return (0);
 }
 int	ft_cd(t_msh *m, char **args)
@@ -24,8 +42,10 @@ int	ft_cd(t_msh *m, char **args)
 }
 int	ft_pwd(t_msh *m, char **args)
 {
-	//arg handling
-	ft_printf("%s\n", ft_getenv(m, "PWD"));
+	if (strslen(args) >= 1)
+		ft_printf("pwd: too many arguments\n");
+	else
+		ft_printf("%s\n", ft_getenv(m, "PWD"));
 	return (0);
 }
 int	ft_unset(t_msh *m, char **args)
@@ -35,8 +55,10 @@ int	ft_unset(t_msh *m, char **args)
 }
 int	ft_env(t_msh *m, char **args)
 {
-	//arg handling
-	prints(m->env);
+	if (strslen(args) >= 1)
+		ft_printf("env: too many arguments\n");
+	else
+		prints(m->env);
 	return (0);
 }
 int	ft_exit(t_msh *m, char **args)
