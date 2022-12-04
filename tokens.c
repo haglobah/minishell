f8d7866a641a23f7e1a4229dbd9389697ccbf732
Beat Hagenlocher <bah@posteo.de>
2022-12-04 19:38:41 +0100

    Norm: tokens.c

--- tokens.c
+++ tokens.c
@@ -6,7 +6,7 @@
 /*   By: bhagenlo <bhagenlo@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 20:17:26 by bhagenlo          #+#    #+#             */
-/*   Updated: 2022/12/04 17:01:57 by bhagenlo         ###   ########.fr       */
+/*   Updated: 2022/12/04 19:32:08 by bhagenlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	print_tokens(t_list *tokens)
 {
-	while(tokens)
+	while (tokens)
 	{
 		ft_printf("%s%%\n", tokens->content);
 		tokens = tokens->next;
@@ -34,14 +34,32 @@ void	print_tokarr(char **toks)
 	ft_printf("\n");
 }
 
-//returns NULL terminated array of strings
+int		alloc_toks(t_list *toks, char ***tok_arr)
+{
+	int	i;
+	int	slen;
+
+	slen = 0;
+	i = 0;
+	while (toks)
+	{
+		slen = ft_strlen(toks->content);
+		(*tok_arr)[i] = (char *)ft_calloc(slen + 1, sizeof(char));
+		if (!(*tok_arr)[i])
+			return (0);
+		ft_strcpy((*tok_arr)[i], toks->content);
+		toks = toks->next;
+		i++;
+	}
+	return (1);
+}
+
+//returns NULL-terminated array of strings
 char	**list_to_arr(t_list *toks)
 {
-	int	slen;
-	int	llen;
+	int		llen;
 	char	**tok_arr;
 	t_list	*cpy;
-	int	i;
 
 	llen = 0;
 	cpy = toks;
@@ -53,17 +71,7 @@ char	**list_to_arr(t_list *toks)
 	tok_arr = (char **)ft_calloc(llen + 1, sizeof(char *));
 	if (!tok_arr)
 		return (NULL);
-	slen = 0;
-	i = 0;
-	while (toks)
-	{
-		slen = ft_strlen(toks->content);
-		tok_arr[i] = (char *)ft_calloc(slen + 1, sizeof(char));
-		if (!(*tok_arr))
-			return (NULL);
-		ft_strcpy(tok_arr[i], toks->content);
-		toks = toks->next;
-		i++;
-	}
+	if (alloc_toks(toks, &tok_arr) == 0)
+		return (NULL);
 	return (tok_arr);
 }
